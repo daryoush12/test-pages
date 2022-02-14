ea93314c12289d7f54ef0a6d0a8d0ef0923083eb
Daryoush Farsimadan <daryoush.farsimadan@ainia.fi>
2022-02-14 10:23:06 +0200

    test jsdoc + betterdocs + js

---
+++ docs/entry.js
@@ -0,0 +1,45 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import Vue from 'vue/dist/vue.js';
+
+      window.Vue = Vue;
+
+
+      import VueWrapper from '../better-docs/lib/vue-wrapper.js';
+
+      window.VueWrapper = VueWrapper;
+
+    
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../src/components/Box.vue';
+vueComponents['Box'] = Component0;
+
+import Component1 from '../src/components/dashName.vue';
+vueComponents['dash-name'] = Component1;
+
+import Component2 from '../src/components/ReactComponent.jsx';
+reactComponents['Documented'] = Component2;
+
+import Component3 from '../src/components/ProgressBar.vue';
+vueComponents['ProgressBar'] = Component3;
